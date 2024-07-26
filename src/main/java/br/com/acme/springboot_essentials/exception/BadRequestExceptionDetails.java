b4a86b4e452f58425b390b00d20294447c50ce0e
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-07-26 18:00:29 -0300

    Adicionado aula sobre handle exception

---
+++ src/main/java/br/com/acme/springboot_essentials/exception/BadRequestExceptionDetails.java
@@ -0,0 +1,14 @@
+package br.com.acme.springboot_essentials.exception;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Getter
+@SuperBuilder
+public class BadRequestExceptionDetails extends ExceptionDetails {
+
+}
--- src/main/java/br/com/acme/springboot_essentials/requests/AnimePostRequestBody.java
+++ src/main/java/br/com/acme/springboot_essentials/requests/AnimePostRequestBody.java
@@ -2,7 +2,12 @@ package br.com.acme.springboot_essentials.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
+
 @Data
 public class AnimePostRequestBody {
+
+    @NotEmpty(message = "The anime name cannot be empty.")
     private String name;
 }
