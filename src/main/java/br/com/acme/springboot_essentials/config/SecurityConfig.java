2011314401f9de6a3cadf1c9b103da554ac19eaf
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-08-12 21:56:43 -0300

    Adicionado actuator

--- src/main/java/br/com/acme/springboot_essentials/config/SecurityConfig.java
+++ src/main/java/br/com/acme/springboot_essentials/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/animes/admin/**").hasRole("ADMIN")
                 .antMatchers("/animes/**").hasRole("USER")
                 .antMatchers("/api-docs/**").hasRole("USER")
+                .antMatchers("/actuator/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
