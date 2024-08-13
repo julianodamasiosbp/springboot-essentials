492cce06422e937fdcf42cc49f2fbb3ee1a93d0a
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-08-13 17:21:02 -0300

    Adicionado grafana no projeto

--- src/main/java/br/com/acme/springboot_essentials/SpringbootEssentialsApplication.java
+++ src/main/java/br/com/acme/springboot_essentials/SpringbootEssentialsApplication.java
@@ -1,7 +1,11 @@
 package br.com.acme.springboot_essentials;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootEssentialsApplication {
@@ -10,4 +14,9 @@ public class SpringbootEssentialsApplication {
 		SpringApplication.run(SpringbootEssentialsApplication.class, args);
 	}
 
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+			@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
+	}
 }
